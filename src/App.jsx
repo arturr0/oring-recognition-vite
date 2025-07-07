c25a2f2368a4e94cfb8624550d6809fb70affc8d
Artur Bednarski <119508496+arturr0@users.noreply.github.com>
2025-07-07 23:32:34 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -525,11 +525,11 @@ function App() {
         
         <div className="status">
           Status: <span className={`status-${status.toLowerCase()}`}>{status}</span>
-          {error && (
+{/*           {error && (
             <div className={`alert ${typeof error === 'string' ? 'alert-error' : 'alert-success'}`}>
               {typeof error === 'string' ? error : error.message}
             </div>
-          )}
+          )} */}
         </div>
       </div>
     </div>
